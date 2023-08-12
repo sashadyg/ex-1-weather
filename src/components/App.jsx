65fa4e1997fc7d242fe1a79736797542baf37687
sashadyg <dyg.sasha12@gmail.com>
2023-08-12 16:58:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import sprite from './utils/svg/sprite.svg';
+
 export const App = () => {
   return (
     <>
@@ -13,7 +15,35 @@ export const App = () => {
         </div>
       </header>
 
-      <main></main>
+      <main>
+        <div className="container">
+          <section className="dayChoose--section">
+            <h1 className="dayChoose--cityName">Cherkassy</h1>
+
+            <ul className="dayChoose--list">
+              <li className="dayChoose--item__active">
+                <div className="dayChoose--itemCard__active">
+                  <h1 className="itemCard--title">Today</h1>
+
+                  <div className="itemCard--info">
+                    <svg className="icon">
+                      <use href={sprite + '#icon-sun'}></use>
+                    </svg>
+                    <div className="itemCard--degrees__position">
+                      <p className="itemCard--degrees itemCard--degrees__max">
+                        19°
+                      </p>
+                      <p className="itemCard--degrees itemCard--degrees__min">
+                        17°
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </main>
     </>
   );
 };
