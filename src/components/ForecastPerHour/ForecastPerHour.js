96666879a4aa37a30a0f745389718031d1ffca4a
sashadyg <dyg.sasha12@gmail.com>
2023-10-03 18:41:18 +0300

    Create ForecastPerHour.js

---
+++ src/components/ForecastPerHour/ForecastPerHour.js
@@ -0,0 +1,11 @@
+import { Card } from './Card';
+
+export const ForecastPerHour = data => {
+  return (
+    <div className="container">
+      <ul className="forecastPerHour--list">
+        <Card />
+      </ul>
+    </div>
+  );
+};
