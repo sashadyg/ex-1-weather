07288d82c8e6a0a817fc135641034fb7ae439dac
sashadyg <dyg.sasha12@gmail.com>
2023-10-10 20:45:16 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,35 @@
-export const Header = () => {
+import { Formik, Field, Form } from 'formik';
+
+export const Header = props => {
   return (
     <header className="header">
       <div className="header--container">
         <p className="header--logo">WEATHER</p>
 
-        <input
+        <Formik
+          initialValues={{
+            cityName: '',
+          }}
+          onSubmit={values => {
+            props.inputCity(values.cityName);
+          }}
+        >
+          <Form className="header--Form">
+            <Field
+              id="cityName"
+              name="cityName"
+              type="text"
+              className="header--input"
+              placeholder="Enter a city"
+              autoComplete="off"
+            />
+          </Form>
+        </Formik>
+        {/* <input
           type="text"
           className="header--input"
           placeholder="Enter a city"
-        />
+        /> */}
       </div>
     </header>
   );
