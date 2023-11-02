e12e0586644567d5b4fa02e41989d0f89e2e70d8
sashadyg <dyg.sasha12@gmail.com>
2023-11-02 22:08:50 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,64 @@
 import { Formik, Field, Form } from 'formik';
+import fetchCities from 'components/fetch/fetchCities';
+import { useState } from 'react';
 
 export const Header = props => {
+  const [citiesName, setCitiesName] = useState([]);
+
+  async function handleChange(e) {
+    const value = e.target.value.trim();
+
+    if (value <= 3) {
+      return;
+    }
+
+    const cities = await fetchCities(value);
+    setCitiesName(cities);
+  }
+
   return (
     <header className="header">
       <div className="header--container">
         <p className="header--logo">WEATHER</p>
 
-        <Formik
-          initialValues={{
-            cityName: '',
-          }}
-          onSubmit={values => {
-            props.inputCity(values.cityName);
-          }}
-        >
-          <Form className="header--Form">
-            <Field
-              id="cityName"
-              name="cityName"
-              type="text"
-              className="header--input"
-              placeholder="Enter a city"
-              autoComplete="off"
-            />
-          </Form>
-        </Formik>
-        {/* <input
-          type="text"
-          className="header--input"
-          placeholder="Enter a city"
-        /> */}
+        <div className="header--search">
+          <Formik
+            initialValues={{
+              cityName: props.cityName,
+            }}
+            onSubmit={values => {
+              console.log(values);
+            }}
+          >
+            <Form className="header--Form">
+              <Field
+                id="cityName"
+                name="cityName"
+                type="text"
+                className="header--input"
+                placeholder="Enter a city"
+                autoComplete="off"
+                onChange={handleChange}
+              />
+            </Form>
+          </Formik>
+
+          <ul>
+            {citiesName ? (
+              citiesName.map(city => {
+                const { name, country } = city;
+
+                return (
+                  <li key={name} className="header--cities">
+                    {name}, {country === 'Russia' ? 'shit' : country}
+                  </li>
+                );
+              })
+            ) : (
+              <></>
+            )}
+          </ul>
+        </div>
       </div>
     </header>
   );
