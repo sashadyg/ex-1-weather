1ad47f1ecd084a9d72144ff3355c01a0cceaedfb
sashadyg <dyg.sasha12@gmail.com>
2023-11-02 22:34:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,7 @@ import { Header } from './Header/Header';
 import { DayChooseSection } from './DayChooseSection/DayChooseSection';
 import { ForecastPerHour } from './ForecastPerHour/ForecastPerHour';
 import { useState } from 'react';
-import fetchForecast from './fetch/fetchForecast';
-import fetchCities from './fetch/fetchCities';
+// import fetchForecast from './fetch/fetchForecast';
 
 export const App = () => {
   const [cityName, setCityName] = useState('');
