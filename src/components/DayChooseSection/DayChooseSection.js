041df78ffaf8eddfe9d11052fb9f8f99c263bb8c
sashadyg <dyg.sasha12@gmail.com>
2023-12-14 17:28:27 +0200

    Update DayChooseSection.js

--- src/components/DayChooseSection/DayChooseSection.js
+++ src/components/DayChooseSection/DayChooseSection.js
@@ -1,9 +1,19 @@
 // import sprite from '../utils/svg/sprite.svg';
 import { ActiveCard } from './ActiveCard/ActiveCard';
 import { CasualCard } from './CasualCard/CasualCard';
+import { useState } from 'react';
 
 export const DayChooseSection = props => {
-  const { forecast } = props;
+  const [currentDay, changeCurrentDay] = useState(0);
+  const forecast = props?.forecast?.forecast?.forecastday;
+
+  const handleClick = e => {
+    // e.preventDefault();
+
+    changeCurrentDay(Number(e.currentTarget.getAttribute('index')));
+  };
+
+  // console.log(forecast);
 
   return (
     <section className="dayChoose--section">
@@ -11,7 +21,24 @@ export const DayChooseSection = props => {
         <h1 className="dayChoose--cityName">{props.cityName}</h1>
 
         <ul className="dayChoose--list">
-          <ActiveCard forecast={forecast} />
+          {forecast?.map((day, index) =>
+            index === currentDay ? (
+              <ActiveCard
+                forecast={forecast}
+                currentDay={currentDay}
+                key={index}
+              />
+            ) : (
+              <CasualCard
+                key={index}
+                onClick={handleClick}
+                index={index}
+                forecast={forecast}
+                currentDay={currentDay}
+              />
+            )
+          )}
+          {/* <ActiveCard forecast={forecast} />
           <CasualCard />
           <CasualCard />
           <CasualCard />
@@ -19,7 +46,7 @@ export const DayChooseSection = props => {
           <CasualCard />
           <CasualCard />
           <CasualCard />
-          <CasualCard />
+          <CasualCard /> */}
         </ul>
       </div>
     </section>
